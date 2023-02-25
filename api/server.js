b0a34e60a4531699174729add97d1c3c5bdb03b9
Sujata-github <sujatanpowar1213@gmail.com>
2023-02-25 18:57:49 +0530

    Initial commit

---
+++ api/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//mongodb+srv://todo-list:<password>@cluster0.mnmb7ek.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://todo-list:H8ctQAJVdkPFeZLg@cluster0.mnmb7ek.mongodb.net/mern?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://todo-list:H8ctQAJVdkPFeZLg@cluster0.mnmb7ek.mongodb.net/mern?retryWrites=true&w=majority', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+
+// Models
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.delete('/todos/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json({result});
+});
+
+app.get('/todos/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
+})
+
+app.put('/todos/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.listen(3003);
